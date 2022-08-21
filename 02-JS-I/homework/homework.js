80a541f82e45822913bba943cff2f629816412a1
daposadap19 <diegoposada1999@gmail.com>
2022-08-21 09:42:38 -0500

    mi segundo cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hola soy Diego";
+const nuevaString = "hola soy Diegoo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
